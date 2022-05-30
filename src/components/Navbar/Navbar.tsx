114c839bf336b15111846d3129bf4745a6ad1296
christopher-alba <calb200@aucklanduni.ac.nz>
2022-05-30 18:15:27 +1200

    set theme in local storage after theme switch

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,41 +2,31 @@ import React, { Dispatch, SetStateAction } from "react";
 import { BrandH1, Button, MainDiv } from "./styled";
 import themes from "../../themes/schema.json";
 
-interface Props {
-  setSelectedTheme: Dispatch<
-    SetStateAction<{
-      name: string;
-      colors: {
-        primary: string;
-        primaryHover: string;
-        secondary: string;
-        secondaryHover: string;
-        tertiary: string;
-        tertiary2: string;
-        tertiary3: string;
-      };
-    }>
-  >;
-  selectedTheme: {
-    name: string;
-    colors: {
-      primary: string;
-      primaryHover: string;
-      secondary: string;
-      secondaryHover: string;
-      tertiary: string;
-      tertiary2: string;
-      tertiary3: string;
-    };
+type Theme = {
+  name: string;
+  colors: {
+    primary: string;
+    primaryHover: string;
+    secondary: string;
+    secondaryHover: string;
+    tertiary: string;
+    tertiary2: string;
+    tertiary3: string;
   };
+};
+
+interface Props {
+  setSelectedTheme: Dispatch<SetStateAction<Theme>>;
+  selectedTheme: Theme;
 }
 
 const Navbar: React.FC<Props> = ({ setSelectedTheme, selectedTheme }) => {
   type voidFunc = () => void;
   const handleClick: voidFunc = () => {
-    setSelectedTheme(
-      selectedTheme.name === "dark" ? themes.data.light : themes.data.dark
-    );
+    const theme: Theme =
+      selectedTheme.name === "dark" ? themes.data.light : themes.data.dark;
+    setSelectedTheme(theme);
+    localStorage.setItem("theme", JSON.stringify(theme));
   };
   return (
     <MainDiv>
