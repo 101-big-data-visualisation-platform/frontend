d27ff2dd4fa780930a0bf277d9f63f209c24a9f8
christopher-alba <calb200@aucklanduni.ac.nz>
2022-08-31 15:30:42 +1200

    update UI to fixed state

--- src/components/graphs/SingleStatistic/SingleStatistic.tsx
+++ src/components/graphs/SingleStatistic/SingleStatistic.tsx
@@ -110,6 +110,7 @@ const SingleStatistic = ({
             }) => {
               return (
                 <SingleStatisticGauge
+                  detailed={true}
                   allDashboards={allDashboards as any}
                   dashboardName={dashboardName}
                   setDashboards={setDashboards}
@@ -136,6 +137,10 @@ const SingleStatistic = ({
           }) => {
             return (
               <SingleStatisticGauge
+                detailed={false}
+                deleting={deleting}
+                setDeleting={setDeleting}
+                graphID={graphID}
                 allDashboards={allDashboards as any}
                 dashboardName={dashboardName}
                 setDashboards={setDashboards}
@@ -152,6 +157,10 @@ const SingleStatistic = ({
 };
 
 const SingleStatisticGauge: FC<{
+  deleting?: boolean;
+  setDeleting?: any;
+  graphID?: string;
+  detailed: boolean;
   allDashboards: Dashboard[];
   dashboardName: string;
   setDashboards: any;
@@ -159,6 +168,10 @@ const SingleStatisticGauge: FC<{
   options: any;
   dataset: any;
 }> = ({
+  deleting,
+  setDeleting,
+  graphID,
+  detailed,
   allDashboards,
   dashboardName,
   setDashboards,
@@ -175,125 +188,260 @@ const SingleStatisticGauge: FC<{
     month: "long",
     day: "numeric",
   };
-  return (
-    <StyledDiv1>
-      <input
-        type="number"
-        placeholder="custom min"
-        onChange={(evt) => setCustomMin(evt.target.value as any)}
-      />
-      <input
-        type="number"
-        placeholder="custom max"
-        onChange={(evt) => setCustomMax(evt.target.value as any)}
-      />
-      <button
-        onClick={async () => {
-          const dashboardsModified = allDashboards?.map((dashboard) => {
-            if (dashboardName === dashboard.name) {
-              return {
-                name: dashboard.name,
-                allGraphs: dashboard.allGraphs.map((graph) => {
-                  return {
-                    ...graph,
-                    datasets: graph.datasets.map((datasetOld) => {
-                      if (datasetOld.ID === dataset.ID) {
-                        return {
-                          ...datasetOld,
-                          singleStatisticMin: parseFloat(customMin as any),
-                          singleStatisticMax: parseFloat(customMax as any),
-                        };
-                      } else {
-                        return datasetOld;
-                      }
-                    }),
-                  };
-                }),
-              };
-            } else {
-              return dashboard;
-            }
-          });
-          await updateUserSettingsAWS(
-            user?.username as any,
-            dashboardsModified as any
-          );
-          setDashboards(dashboardsModified);
-          setRevisionCounter((prevState) => prevState++);
-        }}
-      >
-        Update Min/Max
-      </button>
-      <Plot
-        revision={revisionCounter}
-        data={[
-          {
-            value: parseFloat(
-              parseFloat(
-                dataset.items[dataset.items.length - 1]?.[options.dataSelector]
-              ).toFixed(2)
-            ),
-            gauge: {
-              axis: {
-                range: [
-                  dataset.customMin >= 0
-                    ? dataset.customMin
-                    : (() => {
-                        let min: number = parseFloat(
-                          dataset?.items[0]?.[options.dataSelector]
-                        );
+  if (detailed) {
+    return (
+      <StyledDiv1>
+        <input
+          type="number"
+          placeholder="custom min"
+          onChange={(evt) => setCustomMin(evt.target.value as any)}
+        />
+        <input
+          type="number"
+          placeholder="custom max"
+          onChange={(evt) => setCustomMax(evt.target.value as any)}
+        />
+        <button
+          onClick={async () => {
+            const dashboardsModified = allDashboards?.map((dashboard) => {
+              if (dashboardName === dashboard.name) {
+                return {
+                  name: dashboard.name,
+                  allGraphs: dashboard.allGraphs.map((graph) => {
+                    return {
+                      ...graph,
+                      datasets: graph.datasets.map((datasetOld) => {
+                        if (datasetOld.ID === dataset.ID) {
+                          return {
+                            ...datasetOld,
+                            singleStatisticMin: parseFloat(customMin as any),
+                            singleStatisticMax: parseFloat(customMax as any),
+                          };
+                        } else {
+                          return datasetOld;
+                        }
+                      }),
+                    };
+                  }),
+                };
+              } else {
+                return dashboard;
+              }
+            });
+            await updateUserSettingsAWS(
+              user?.username as any,
+              dashboardsModified as any
+            );
+            setDashboards(dashboardsModified);
+            setRevisionCounter((prevState) => prevState++);
+          }}
+        >
+          Update Min/Max
+        </button>
+        <Plot
+          revision={revisionCounter}
+          data={[
+            {
+              value: parseFloat(
+                parseFloat(
+                  dataset.items[dataset.items.length - 1]?.[
+                    options.dataSelector
+                  ]
+                ).toFixed(2)
+              ),
+              gauge: {
+                axis: {
+                  range: [
+                    dataset.customMin >= 0
+                      ? dataset.customMin
+                      : (() => {
+                          let min: number = parseFloat(
+                            dataset?.items[0]?.[options.dataSelector]
+                          );
 
-                        for (let i = 1; i < dataset.items.length; i++) {
-                          if (dataset?.items[i]?.[options.dataSelector] < min) {
-                            min = parseFloat(
-                              dataset?.items[i]?.[options.dataSelector]
-                            );
+                          for (let i = 1; i < dataset.items.length; i++) {
+                            if (
+                              dataset?.items[i]?.[options.dataSelector] < min
+                            ) {
+                              min = parseFloat(
+                                dataset?.items[i]?.[options.dataSelector]
+                              );
+                            }
                           }
-                        }
-                        return parseFloat(min.toFixed(2));
-                      })(),
-                  dataset.customMax >= 0
-                    ? dataset.customMax
-                    : (() => {
-                        let max = parseFloat(
-                          dataset?.items[0]?.[options.dataSelector]
-                        );
-                        for (let i = 1; i < dataset.items.length; i++) {
-                          if (dataset?.items[i]?.[options.dataSelector] > max) {
-                            max = parseFloat(
-                              dataset?.items[i]?.[options.dataSelector]
-                            );
+                          return parseFloat(min.toFixed(2));
+                        })(),
+                    dataset.customMax >= 0
+                      ? dataset.customMax
+                      : (() => {
+                          let max = parseFloat(
+                            dataset?.items[0]?.[options.dataSelector]
+                          );
+                          for (let i = 1; i < dataset.items.length; i++) {
+                            if (
+                              dataset?.items[i]?.[options.dataSelector] > max
+                            ) {
+                              max = parseFloat(
+                                dataset?.items[i]?.[options.dataSelector]
+                              );
+                            }
                           }
-                        }
-                        return parseFloat(max.toFixed(2));
-                      })(),
-                ],
-                tickwidth: 1,
-                tickcolor: "darkblue",
+                          return parseFloat(max.toFixed(2));
+                        })(),
+                  ],
+                  tickwidth: 1,
+                  tickcolor: "darkblue",
+                },
+                bar: { color: "darkblue" },
+                bgcolor: "white",
+                borderwidth: 2,
+                bordercolor: "gray",
               },
-              bar: { color: "darkblue" },
-              bgcolor: "white",
-              borderwidth: 2,
-              bordercolor: "gray",
+              title: {
+                text: options.datasetOptions.find(
+                  (option: { dataName: any }) =>
+                    option.dataName === dataset.name
+                )?.label,
+              },
+              type: "indicator",
+              mode: "gauge+number",
             },
-            title: {
-              text: options.datasetOptions.find(
-                (option: { dataName: any }) => option.dataName === dataset.name
-              )?.label,
+          ]}
+          layout={{ width: 600, height: 500, margin: { t: 0, b: 0 } }}
+        />
+        <p>
+          {new Date(
+            parseInt(dataset.items[dataset.items.length - 1]?.timeStamp)
+          ).toLocaleDateString("en-US", dateOptions)}
+        </p>
+      </StyledDiv1>
+    );
+  } else {
+    return (
+      <StyledDiv1>
+        <div style={{ width: "300px", textAlign: "center" }}>
+          <h1 style={{ marginBottom: "0", fontSize: "1.5rem" }}>
+            {options.graphTitleText}
+          </h1>
+          <p style={{ marginTop: "0" }}>latest {options.dataSelector} data</p>
+        </div>
+        <StyledLink
+          to={`/detailed?dataSelector=${options.dataSelector}&graphID=${graphID}`}
+        >
+          Detailed View
+        </StyledLink>
+        <StyledButton
+          onClick={async () => {
+            setDeleting(true);
+
+            const dashboardsModified = allDashboards?.map((dashboard) => {
+              if (dashboardName === dashboard.name) {
+                return {
+                  name: dashboard.name,
+                  allGraphs: dashboard.allGraphs.filter(
+                    (graph) => graph.graphID !== graphID
+                  ),
+                };
+              } else {
+                return dashboard;
+              }
+            });
+
+            await updateUserSettingsAWS(
+              user?.username || "",
+              dashboardsModified
+            );
+
+            setDeleting(false);
+            setDashboards(dashboardsModified);
+          }}
+          disabled={deleting}
+        >
+          Delete
+        </StyledButton>
+        {deleting && (
+          <LinearProgress
+            style={{ marginTop: "10px", width: "300px" }}
+            color="inherit"
+          />
+        )}
+        <Plot
+          revision={revisionCounter}
+          data={[
+            {
+              value: parseFloat(
+                parseFloat(
+                  dataset.items[dataset.items.length - 1]?.[
+                    options.dataSelector
+                  ]
+                ).toFixed(2)
+              ),
+              gauge: {
+                axis: {
+                  range: [
+                    dataset.customMin
+                      ? dataset.customMin >= 0
+                      : (() => {
+                          let min: number = parseFloat(
+                            dataset?.items[0]?.[options.dataSelector]
+                          );
+
+                          for (let i = 1; i < dataset.items.length; i++) {
+                            if (
+                              dataset?.items[i]?.[options.dataSelector] < min
+                            ) {
+                              min = parseFloat(
+                                dataset?.items[i]?.[options.dataSelector]
+                              );
+                            }
+                          }
+                          return parseFloat(min.toFixed(2));
+                        })(),
+                    dataset.customMax >= 0
+                      ? dataset.customMax
+                      : (() => {
+                          let max = parseFloat(
+                            dataset?.items[0]?.[options.dataSelector]
+                          );
+                          for (let i = 1; i < dataset.items.length; i++) {
+                            if (
+                              dataset?.items[i]?.[options.dataSelector] > max
+                            ) {
+                              max = parseFloat(
+                                dataset?.items[i]?.[options.dataSelector]
+                              );
+                            }
+                          }
+                          return parseFloat(max.toFixed(2));
+                        })(),
+                  ],
+                  tickwidth: 1,
+                  tickcolor: "darkblue",
+                },
+                bar: { color: "darkblue" },
+                bgcolor: "white",
+                borderwidth: 2,
+                bordercolor: "gray",
+              },
+              title: {
+                text: options.datasetOptions.find(
+                  (option: { dataName: any }) =>
+                    option.dataName === dataset.name
+                )?.label,
+              },
+              type: "indicator",
+              mode: "gauge+number",
             },
-            type: "indicator",
-            mode: "gauge+number",
-          },
-        ]}
-        layout={{ width: 600, height: 500, margin: { t: 0, b: 0 } }}
-      />
-      <p>
-        {new Date(
-          parseInt(dataset.items[dataset.items.length - 1]?.timeStamp)
-        ).toLocaleDateString("en-US", dateOptions)}
-      </p>
-    </StyledDiv1>
-  );
+          ]}
+          layout={{ width: 600, height: 500, margin: { t: 0, b: 0 } }}
+        />
+        <p>
+          {new Date(
+            parseInt(dataset.items[dataset.items.length - 1]?.timeStamp)
+          ).toLocaleDateString("en-US", dateOptions)}
+        </p>
+      </StyledDiv1>
+    );
+  }
 };
 
 export default SingleStatistic;
